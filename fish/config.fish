07a7106e6dda7f22f5dc3115dc5c79bcbfd9b22f
Buddy Magsipoc <Hanekawa@Buddys-MacBook-Pro.local>
2015-12-28 13:41:40 +0800

    +Fish。

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# Path to Oh My Fish install.
+set -gx OMF_PATH /Users/Hanekawa/.local/share/omf
+
+# Customize Oh My Fish configuration path.
+#set -gx OMF_CONFIG /Users/Hanekawa/.config/omf
+
+# Load oh-my-fish configuration.
+source $OMF_PATH/init.fish
