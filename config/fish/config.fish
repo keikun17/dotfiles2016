7d1c109b878ca69026f9302866ea993521171681
Buddy Magsipoc <keikun17@gmail.com>
2016-03-03 14:36:25 +0800

    GTFO OMF。

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,3 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH /Users/Hanekawa/.local/share/omf
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG /Users/Hanekawa/.config/omf
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 # Load my Aliases
 source ~/.config/fish/aliases.fish
 
