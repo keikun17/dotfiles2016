561cd92a92f742e0ce04d7234885e15837c582c9
Buddy Magsipoc <keikun17@gmail.com>
2018-07-01 15:32:35 +0800

    Fish user paths。

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -7,3 +7,5 @@ source ~/.config/fish/aliases.fish
 for filename in $HOME/.config/fish/config.d/*.fish
   source $filename
 end
+set -g fish_user_paths "/usr/local/opt/icu4c/bin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/icu4c/sbin" $fish_user_paths
